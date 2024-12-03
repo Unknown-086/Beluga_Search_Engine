5d992cb9eca2698f3fae7de8d335f368e57b8b13
Unknown <147093562+Unknown-086@users.noreply.github.com>
2024-12-03 18:23:35 +0500

    Testing on Sample Datasets

--- src/InvertedIndex/InvertedIndxmakerTest.py
+++ src/InvertedIndex/InvertedIndxmakerTest.py
@@ -4,12 +4,12 @@ from src.InvertedIndex.InvertedIndexBuilder import buildInvertedIndex
 
 # Main execution
 if __name__ == "__main__":
-    forwardIndexPath = os.path.join('../..', 'data', 'ForwardIndexData', 'ForwardIndex_GlobalNews_English2.json')
-    invertedIndexPath = os.path.join('../..', 'data', 'InvertedIndexData', 'InvertedIndex_GlobalNews_English2.json')
+    forwardIndexPath = os.path.join('../..', 'data', 'ForwardIndexData', 'SampleTesting', 'ForwardIndex_1000.json')
+    invertedIndexPath = os.path.join('../..', 'data', 'InvertedIndexData', 'SampleTesting', 'InvertedIndex_1000.json')
 
     start = time.time()
     print("Building inverted index...")
     buildInvertedIndex(forwardIndexPath, invertedIndexPath)
     end = time.time()
     print(f"Inverted index built and saved successfully.")
-    print(f"Time taken: {end - start:.2f} seconds")
+    print(f"Time taken: {end - start:.6f} seconds")
--- src/Lexicon/LexiconMakerTest.py
+++ src/Lexicon/LexiconMakerTest.py
@@ -5,22 +5,28 @@ from src.Lexicon.LexiconBuilder import saveLexiconToJSON, buildLexicon
 
 # Test file for LexiconBuilder.py
 if __name__ == "__main__":
-    datasetsPaths = [
-        os.path.join('../..', 'data', 'FilteredDatasets', 'ModifiedGlobalNewsDataset13_English.csv')
+    datasetPaths = [
+        os.path.join('../..', 'data', 'SampleDatasets_ForTesting', 'GlobalNewsDataset_Sample_1000.csv'),
+        os.path.join('../..', 'data', 'SampleDatasets_ForTesting', 'RedditDataset_Sample_1000.csv'),
+        os.path.join('../..', 'data', 'SampleDatasets_ForTesting', 'WeeklyNewsDataset_Aug17_1000.csv'),
+        os.path.join('../..', 'data', 'SampleDatasets_ForTesting', 'WeeklyNewsDataset_Aug18_1000.csv')
     ]
 
     columnLists = [
-        [ 'source_name', 'author', 'title', 'description', 'content', 'category', 'full_content']
+        [ 'published_at', 'source_name', 'author', 'title', 'description', 'content', 'category', 'full_content' ],
+        [ 'date', 'subreddit', 'title', 'author', 'domain' ],
+        [ 'publish_time', 'feed_code', 'headline_text'] ,
+        [ 'publish_time', 'feed_code', 'headline_text' ]
     ]
 
     startTime = time.time()
-    lexiconOutputPath = os.path.join('../..', 'data', 'Lexicons', 'LexiconModifiedGlobalNewsDataset13_7_8.json')
+    lexiconOutputPath = os.path.join('../..', 'data', 'Lexicons', 'SampleTesting', 'Lexicon_1000.json')
 
     print("Building the combined lexicon...")
-    lexicon = buildLexicon(datasetsPaths, columnLists)
+    lexicon = buildLexicon(datasetPaths, columnLists)
 
     print(f"Saving the combined lexicon to {lexiconOutputPath}...")
     saveLexiconToJSON(lexicon, lexiconOutputPath)
     endTime = time.time()
     print(f"Combined lexicon built and saved successfully. Total unique words: {len(lexicon)}")
-    print(f"Time taken: {endTime - startTime:.2f} seconds")
+    print(f"Time taken: {endTime - startTime:.6f} seconds")
